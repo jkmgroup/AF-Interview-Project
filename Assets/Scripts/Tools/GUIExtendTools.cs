13f96c91fd1f91a015f5ba605ac9ceb4c6dc4c00
jkmgroup <jkmgroup1979@gmail.com>
2024-03-01 08:06:37 +0100

    Tools class update
    - update GameTools
    - add GUIExtendTools

---
+++ Assets/Scripts/Tools/GUIExtendTools.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AFSInterview
+{
+	public class GUIExtendTools
+	{
+		static public string TextField(string lable, string text)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(lable);
+			var rezult = GUILayout.TextField(text);
+			GUILayout.EndHorizontal();
+			return rezult;
+		}
+
+		static public int IntField(string lable, int value)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(lable);
+			var rezult = EditorGUILayout.IntField(value);
+			GUILayout.EndHorizontal();
+			return rezult;
+		}
+
+		static public int PopupField(string lable, int selected, string[] values)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(lable);
+			var rezult = EditorGUILayout.Popup(selected, values);
+			GUILayout.EndHorizontal();
+			return rezult;
+		}
+	}
+}
--- Assets/Scripts/Tools/GameTools.cs
+++ Assets/Scripts/Tools/GameTools.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -14,9 +13,30 @@ namespace AFSInterview
 			Debug.Break();
 		}
 
-		internal static Vector3 CreateRandVectorXZ(Vector3 min, Vector3 max, float y = 0.0f)
+		static public Vector3 CreateRandVectorXZ(Vector3 min, Vector3 max, float y = 0.0f)
 		{
 			return new Vector3(Random.Range(min.x, max.x), y, Random.Range(min.z, max.z));
+		}
+
+		static public List<T> ArrayToList<T>(T[] items)
+		{
+			var rezult = new List<T>();
+			foreach (var item in items)
+				rezult.Add(item);
+			return rezult;
+		}
+
+		static public T1[] CreateConvertedArray<T1,T2>(T2[] values, Func<T2, T1> createNew)
+		{
+			var rezult = new T1[values.Length];
+			for (var i = 0; i < rezult.Length; i++)
+				rezult[i] = createNew(values[i]);
+			return rezult;
+		}
+
+		static public bool InRange<T>(List<T> items, int index)
+		{
+			return index > -1 && index < items.Count;
 		}
 	}
 }
